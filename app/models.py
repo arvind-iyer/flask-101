808f6810bc2e63aa62470c4b1d58c1b7f53cc3b6
Arvind Iyer <arvindi.me@gmail.com>
2016-02-03 20:28:08 +0800

    Login with the good old openid

--- app/models.py
+++ app/models.py
@@ -6,6 +6,23 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)
+        except NameError:
+            return str(self.id)
     def __repr__(self):
         return '<User %r>' % (self.nickname)
 
@@ -18,5 +35,3 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post %r>' % (self.body)
-
-        
